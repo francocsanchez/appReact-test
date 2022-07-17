59551e77522d4e75f1baa86b739d0430ae0e9bac
Franco C Sanchez <francocsanchez@gmail.com>
2022-07-17 11:44:01 -0300

    add list products

--- mi-carrito/src/App.js
+++ mi-carrito/src/App.js
@@ -1,6 +1,15 @@
 import { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    products: [
+      { name: 'Tomate', price: 1500, img: '/products/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/products/arbejas.jpg' },
+      { name: 'Lechuga', price: 2000, img: '/products/lechuga.jpg' },
+    ]
+  }
+
   render() {
     return (
       <div>
