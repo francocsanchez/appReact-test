f935c72d658933aa598ec6265ae486b468366e40
Franco C Sanchez <francocsanchez@gmail.com>
2022-07-17 12:49:42 -0300

    add styles

--- mi-carrito/src/componets/Products.js
+++ mi-carrito/src/componets/Products.js
@@ -1,11 +1,19 @@
 import { Component } from "react";
 import Product from './Product'
 
+const styles = {
+    products: {
+        display: 'flex',
+        flexDirecction: 'row',
+        justifyContent: 'space-between'
+    }
+}
+
 class Products extends Component {
     render() {
         const { products, agregarItems } = this.props;
         return (
-            <div>
+            <div style={styles.products}>
                 {products.map((product, i) =>
                     <Product
                         agregarItems={agregarItems}
